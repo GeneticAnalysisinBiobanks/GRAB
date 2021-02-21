c23851bc0d2d737169eb372de9dc585b92be6d13
WenjianBI <38364280+WenjianBI@users.noreply.github.com>
2021-02-20 21:22:35 -0600

    update Geno.R

--- R/Geno.R
+++ R/Geno.R
@@ -3,26 +3,49 @@
 ## The package supports the following genotype format
 ## 1. plink files: bedFile as GenoFile, c(bimFile, famFile) as GenoFileIndex
 
-#' Read genotype data into R
+#' Read genotype data into R as a matrix
 #' 
-#' GRAB package provides functions to read genotype data from multiple formats (including plink) into R
+#' GRAB package provides functions to read in genotype data from multiple format (including Plink, BGEN) into R
 #' 
-#' @param GenoFile a character of genotype file. Three types of genotype files are supported: PLINK ("prefix.bed"), BGEN ("prefix.bgen"), and VCF ("prefix.vcf" or "prefix.vcf.gz"). 
-#' @param GenoFileIndex additional index files corresponding to the "GenoFile". The default is NULL, that is, to share the same prefix as GenoFile. PLINK: c("prefix.bim", "prefix.fam"), BGEN: c("prefix.bgi"), and VCF: c("prefix.vcf.tbi") or c("prefix.vcf.gz.tbi").
+#' @param GenoFile a character of genotype file. See \code{Details} section for more information.
+#' @param GenoFileIndex additional index file(s) corresponding to the \code{GenoFile}. See \code{Details} section for more information.
 #' @param SampleIDs a character vector of sample IDs to extract. The default is NULL, that is, to use all samples in GenoFile.
 #' @param MarkerIDs a character vector of marker IDs to extract. The default is NULL, the first 10 markers will be extracted.
 #' @return An R matrix, each row is for one sample and each column is for one marker.
+#' @details
+#' We support three genotype format including Plink, BGEN, and VCF.
+#' The program will check the format based on the filename extension.  
+#' If \code{GenoFileIndex} is NULL (default), then it uses the same prefix as \code{GenoFile}.
+#' \describe{
+#'   \item{Plink}{\code{GenoFile}: "prefix.bed"; \code{GenoFileIndex}: c("prefix.bim", "prefix.fam")}
+#'   \item{BGEN}{\code{GenoFile}: "prefix.bgen"; \code{GenoFileIndex}: "prefix.bgen.bgi"}
+#'   \item{VCF}{Not available now. \code{GenoFile}: "prefix.vcf"; \code{GenoFileIndex}: "prefix.vcf.tbi"}
+#' }
 #' @examples
-#' PlinkFile = system.file("extdata", "example.bed", package = "GRAB")
-#' GenoMat = GRAB.ReadGeno(PlinkFile)
-#' head(GenoMat)
 #' 
+#' ## The below is raw data 
 #' RawFile = system.file("extdata", "example.raw", package = "GRAB")
 #' GenoMat = data.table::fread(RawFile)
 #' head(GenoMat[,1:15])
-#'       
+#' 
+#' ## The below is Plink format
+#' PlinkFile = system.file("extdata", "example.bed", package = "GRAB")
+#' GenoList = GRAB.ReadGeno(PlinkFile)
+#' GenoMat = GenoList$GenoMat
+#' markerInfo = GenoList$markerInfo
+#' head(GenoMat)
+#' markerInfo
+#' 
+#' ## The below is BGEN format
+#' BGENFile = system.file("extdata", "example_bgen_1.2_8bits.bgen", package = "GRAB")
+#' GenoList = GRAB.ReadGeno(BGENFile)
+#' GenoMat = GenoList$GenoMat
+#' markerInfo = GenoList$markerInfo
+#' head(GenoMat)
+#' markerInfo
+#' 
 #' @export
-#' @import data.table
+#' @import data.table, tidyr, dbplyr, RSQLite
 GRAB.ReadGeno = function(GenoFile,
                          GenoFileIndex = NULL,
                          SampleIDs = NULL,
@@ -31,30 +54,27 @@ GRAB.ReadGeno = function(GenoFile,
   objGeno = setGenoInput(GenoFile, GenoFileIndex, SampleIDs)
   
   genoType = objGeno$genoType
-  markers = objGeno$markers
-  samples = objGeno$samples
-  
-  if(is.null(SampleIDs)){
-    print("Since 'SampleIDs' not specified, we use all samples in 'GenoFile'.")
-    SampleIDs = samples 
-  }
+  markerInfo = objGeno$markerInfo
+  SampleIDs = objGeno$samples
   
   if(is.null(MarkerIDs)){
     print("Since 'MarkerIDs' not specified, we use the first 10 markers in 'GenoFile'.")
-    MarkerIDs = markers[1:min(10,length(markers))]
+    markerInfo = markerInfo[1:min(10,nrow(markerInfo)),]
+    MarkerIDs = markerInfo$ID
   }
   
-  if(any(!SampleIDs %in% samples))
-    stop("At least one sample from 'SampleIDs' are not in 'GenoFile' and 'GenoFileIndex'.")
-  
-  if(any(!MarkerIDs %in% markers))
+  posMarker = match(MarkerIDs, markerInfo$ID, 0)
+  if(any(posMarker == 0))
     stop("At least one marker from 'MarkerIDs' are not in 'GenoFile' and 'GenoFileIndex'.")
+  
+  markerInfo = markerInfo[posMarker, ,drop=F]
   
   GenoMat = getGenoInCPP(MarkerIDs)  # for more details about getGenoInCPP, please check Main.cpp
   colnames(GenoMat) = MarkerIDs;
   rownames(GenoMat) = SampleIDs;
   
-  return(GenoMat)
+  return(list(GenoMat = GenoMat,
+              markerInfo = markerInfo))
 }
 
 ## add something in setGenoInput(.) to select specific markers requested by users
@@ -70,20 +90,24 @@ setGenoInput = function(GenoFile,
   
   GenoFileExt = tools::file_ext(GenoFile);
   
-  if(GenoFileExt != "bed")
-    stop("Current version only supports plink file input (file extension should be .bed).")  # support more genotype input later
+  # support more genotype format later
+  if(GenoFileExt != "bed" & GenoFileExt != "bgen")
+    stop("Current version only supports genotype input of Plink (filename extension of '.bed') or BGEN (filename extension of '.bgen').")  
   
   ########## ----------  Plink format ---------- ##########
   
   if(GenoFileExt == "bed"){
+    
     genoType = "PLINK"
+    
     if(is.null(GenoFileIndex)){  
-      # If GenoFileIndex is not given, we use the same prefix for bim and fam files
-      GenoFileIndex = c(gsub("bed$","bim",GenoFile),
-                        gsub("bed$","fam",GenoFile))
+      # If 'GenoFileIndex' is not given, we use the same prefix for 'bim' and 'fam' files
+      GenoFileIndex = c(gsub("bed$", "bim", GenoFile),
+                        gsub("bed$", "fam", GenoFile))
     }
+    
     if(length(GenoFileIndex) != 2)
-      stop("If plink file is used, argument 'GenoFileIndex' should be 'NULL' or a character vector of c(bimFile, famFile).")
+      stop("If Plink format is used, argument 'GenoFileIndex' should be 'NULL' or a character vector of c(bimFile, famFile).")
     
     bimFile = GenoFileIndex[1]
     famFile = GenoFileIndex[2]
@@ -91,26 +115,72 @@ setGenoInput = function(GenoFile,
     
     if(!file.exists(bimFile)) stop(paste("Cannot find bim file of", bimFile))
     markerInfo = data.table::fread(bimFile)
-    markers = markerInfo$V2
+    markerInfo = as.data.frame(markerInfo)
+    markerInfo = markerInfo[,c(1,4,2,6,5)]  # https://www.cog-genomics.org/plink/2.0/formats#bim
     
     if(!file.exists(famFile)) stop(paste("Cannot find fam file of", famFile))
     sampleInfo = data.table::fread(famFile)
-    samples = sampleInfo$V2
-    
-    if(is.null(SampleIDs))
-      SampleIDs = samples;
-    
+    samplesInGeno = sampleInfo$V2
+    SampleIDs = updateSampleIDs(SampleIDs, samplesInGeno)
+      
     setPLINKobjInCPP(bimFile, famFile, bedFile, SampleIDs)
   }
   
-  ########## ----------  More formats such as BGEN and VCF ---------- ##########
+  ########## ----------  BGEN format ---------- ##########
+  
+  if(GenoFileExt == "bgen"){
+    
+    genoType = "BGEN"
+    
+    if(is.null(GenoFileIndex)){  
+      # If 'GenoFileIndex' is not given, we use the same prefix for 'bgen.bgi' file
+      GenoFileIndex = gsub("bgen$", "bgen.bgi", GenoFile)
+    }
+    
+    if(length(GenoFileIndex) != 1)
+      stop("If BGEN format is used, argument 'GenoFileIndex' should be 'NULL' or a character of bgiFile.")
+    
+    bgiFile = GenoFileIndex[1]
+    bgenFile = GenoFile
+    
+    if(!file.exists(bgiFile)) stop(paste("Cannot find bgi file of", bgiFile))
+    
+    db_con <- RSQLite::dbConnect(RSQLite::SQLite(), bgiFile)
+    on.exit(RSQLite::dbDisconnect(db_con), add = TRUE)
+    bgiData = dplyr::tbl(db_con, "Variant")
+    bgiData = as.data.frame(bgiData)
+    
+    markerInfo = bgiData[,c(1,2,3,6,5)]  # https://www.well.ox.ac.uk/~gav/bgen_format/spec/v1.2.html
+    
+    samplesInGeno = getSampleIDsFromBGEN(bgenFile)
+    SampleIDs = updateSampleIDs(SampleIDs, samplesInGeno)
+    
+    # setPLINKobjInCPP(bimFile, famFile, bedFile, SampleIDs)
+  }
+  
+  ########## ----------  More format such as BGEN and VCF ---------- ##########
+  
+  colnames(markerInfo) = c("CHROM", "POS", "ID", "REF", "ALT")
   
   # return genotype
   print(paste("Based on the 'GenoFile' and 'GenoFileIndex',", genoType, "format is used for genotype data."))
   
-  genoList = list(genoType = genoType, markers = markers, samples = samples)
+  genoList = list(genoType = genoType, markerInfo = markerInfo, SampleIDs = SampleIDs)
   
   return(genoList)
+}
+
+updateSampleIDs = function(SampleIDs, samplesInGeno)
+{
+  if(is.null(SampleIDs)){
+    print("Since 'SampleIDs' not specified, we use all samples in 'GenoFile'.")
+    SampleIDs = samplesInGeno;
+  }
+  
+  if(any(!SampleIDs %in% samplesInGeno))
+    stop("At least one sample from 'SampleIDs' are not in 'GenoFile' and 'GenoFileIndex'.")
+  
+  return(SampleIDs)
 }
 
 ## split 'markers' into multiple chunks each of which includes no more than 'nMarkersEachChunk' markers
@@ -137,5 +207,27 @@ splitMarker = function(markers, nMarkersEachChunk)
   
   return(markerList)
 }
+
+# https://www.well.ox.ac.uk/~gav/bgen_format/spec/v1.2.html
+getSampleIDsFromBGEN = function(bgenFile)
+{
+  con = file(bgenFile, "rb")
+  seek(con, 4)
+  LH = readBin(con, n = 1, what = "integer", size = 4)
+  seek(con, 4 + LH + 4)
+  N = readBin(con, n = 1, what = "integer", size = 4)  # number of samples
+  samplesInGeno = rep(0, N)
+  
+  # cycle for all samples to extract IDs
+  for(i in 1:N){
+    LS = readBin(con, n = 1, what = "integer", size = 2)
+    sample = readChar(con, nchars = LS)
+    samplesInGeno[i] = sample
+  }
+  
+  return(samplesInGeno)
+}
+
+
 
 
