fe6832176599643325f8330d803726f92be4762b
Wenjian Bi <wenjianb@pku.edu.cn>
2021-08-18 16:05:03 +0800

    Update GRAB_Region.R

--- R/GRAB_Region.R
+++ R/GRAB_Region.R
@@ -98,7 +98,7 @@ GRAB.Region = function(objNull,
     # main function to calculate summary statistics for region-based analysis 
     obj.mainRegion = mainRegion(NullModelClass, genoType, genoIndex, OutputFile, n)
     
-    if(is.null(obj.mainRegion)){
+    if(length(obj.mainRegion) == 0){
       next;
     }
     
