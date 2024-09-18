50319249fcba0dac302216aa8f379919ea163ea3
Xu He <81684439+Fantasy-XuHe@users.noreply.github.com>
2024-09-18 16:08:50 +0800

    add SPAGRMGE

--- R/SPAGRM.R
+++ R/SPAGRM.R
@@ -136,7 +136,13 @@ SPAGRM.NullModel = function(ResidMatFile,    # two columns: column 1 is subjID, 
                                            MaxNuminFam = 5,
                                            MAF_interval = c(0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5)))
 {
-  ResidMat = data.table::fread(ResidMatFile)
+  if(is.data.frame(ResidMatFile))
+  {
+    ResidMat = ResidMatFile
+  }else
+  {
+    ResidMat = data.table::fread(ResidMatFile)
+  }
   SparseGRM = data.table::fread(SparseGRMFile)
   PairwiseIBD = data.table::fread(PairwiseIBDFile)
   
@@ -169,7 +175,7 @@ SPAGRM.NullModel = function(ResidMatFile,    # two columns: column 1 is subjID, 
   
   if(ControlOutlier)
   {
-    cat("By default, ControlOutlier = TRUE to keep the outliers within a certain range to improve computational efficiency.\n")
+    cat("ControlOutlier = TRUE (default) to keep the outliers < 5%;\nSet ControlOutlier = FALSE for higher accuracy.\n")
     
     while(sum(ResidMat$Outlier) == 0)
     {
@@ -409,6 +415,225 @@ SPAGRM.NullModel = function(ResidMatFile,    # two columns: column 1 is subjID, 
   class(obj) = "SPAGRM_NULL_Model"
   
   return(obj)
+}
+
+SPAGRMGE.NullModel = function(NullModel = NULL,   # a fitted null model from lme4.
+                              PhenoFile,          # a file path to read in the phenotype.
+                              SubjIDColname,      # a character to specifie the column name of the subject ID.
+                              PhenoColname,       # a character to specifie the column name of the phenotype.
+                              CovaColname,        # a character (vector) to specifie the column name of the covariates (except for Envcolname).
+                              Envcolname,         # a character to specifie the column name of the environment variable.
+                              PlinkFile,          # a PLINK file path to read in some genotypes (without file suffix like ".bim", "bed" or "fam").
+                              SparseGRMFile,      # a path of SparseGRMFile get from getSparseGRM() function.
+                              PairwiseIBDFile,    # a path of PairwiseIBDFile get from getPairwiseIBD() function.
+                              control = list())   # control command used in 'SPAGRM.NullModel', see also 'SPAGRM.NullModel'.
+{
+  if(is.data.frame(PhenoFile))
+  {
+    Pheno_data = PhenoFile
+  }else
+  {
+    Pheno_data = data.table::fread(PhenoFile)
+  }
+  
+  cmd = paste0("Pheno_data = Pheno_data %>% arrange(", SubjIDColname, ")"); eval(parse(text = cmd))
+  
+  if(is.null(NullModel))
+  {
+    cat("Model formula is...\n")
+    model_formula = as.formula(paste(PhenoColname, "~", paste(CovaColname, collapse = "+"), "+", Envcolname, "+ (", Envcolname, "|", SubjIDColname, ")"))
+    print(model_formula)
+    
+    cat("Fit the null model...\n")
+    null_model = lme4::lmer(model_formula, data = Pheno_data)
+  }else
+  {
+    null_model = NullModel
+  }
+  
+  cat("Process the null model product...\n")
+  
+  # Extract variance components and compute penalty matrix (P)
+  varcor = lme4::VarCorr(null_model)
+  cmd = paste0("varcor$", SubjIDColname); G = eval(parse(text = cmd))
+  sig = attr(varcor, "sc")
+  P = solve(G / sig ^ 2)
+  
+  # Put data in convenient arrays and vector
+  cmd = paste0("unique(Pheno_data$", SubjIDColname, ")"); SubjID = eval(parse(text = cmd)); SubjID = as.character(SubjID)
+  cmd = paste0("table(Pheno_data$", SubjIDColname, ")"); k = eval(parse(text = cmd))
+  n = length(SubjID)
+  k = k[SubjID]
+  XY = bind_cols(intercept = 1, Pheno_data %>% select(all_of(Envcolname)), 
+                 Pheno_data %>% select(all_of(CovaColname)), Pheno_data %>% select(all_of(PhenoColname)))
+  XY = as.matrix(XY)
+  TT = XY[, 1:2]
+  nxy = ncol(XY)
+  X = XY[, -nxy]
+  nx = ncol(X)
+  y = XY[, nxy]
+  ncov = nx - ncol(TT)
+  
+  # Compute components of block-diagonal system with covariates  (without SNP)
+  # Additionally compute and store object SS = Rot %*% Si, which is used later on
+  A21 = matrix(NA, 2*n, 2 + ncov)
+  q2 = rep(NA, 2*n)
+  SS = matrix(NA, 2*n, 2)
+  
+  uk = 0
+  for(i in 1:n)
+  {
+    ki = k[i]
+    uk = max(uk) + 1:ki
+    u2 = (i - 1)*2 + 1:2
+    Ti = TT[uk,]
+    if(ki == 1) Ti = t(as.matrix(Ti))
+    Si = crossprod(Ti, Ti)
+    sv = svd(Si + P)
+    Rot = sqrt(1/sv$d) * sv$u
+    Q = Rot %*% t(Ti)
+    SS[u2,] = Rot %*% Si
+    A21[u2,] = Q %*% X[uk, ]
+    q2[u2] = Q %*% y[uk]
+  }
+  
+  q1 = crossprod(X, y)
+  A11 = crossprod(X)
+  Q = A11 - crossprod(A21)
+  q = q1 - crossprod(A21, q2)
+  sol = solve(Q, q)
+  blups = q2 - A21 %*% sol
+  
+  # Compute sums of products per subject involved in the crossprod(X, G), crossprod(G) and crossprod(G, y).
+  ex = matrix(1, 1, ncov + 2)
+  et = matrix(1, 1, 2)
+  XTk = kronecker(et, X) * kronecker(TT, ex)
+  TTk = kronecker(et, TT) * kronecker(TT, et)
+  Tyk = y * TT
+  XTs = matrix(0, n, ncol(XTk))
+  TTs = matrix(0, n, ncol(TTk))
+  Tys = matrix(0, n, 2)
+  AtS = matrix(0, n, 2 * nx)
+  
+  uk = 0
+  for (i in 1:n) 
+  {
+    ki = k[i]
+    uk = max(uk) + 1:ki
+    if(ki == 1)
+    {
+      XTs[i, ] = XTk[uk, ]
+      TTs[i, ] = TTk[uk, ]
+      Tys[i, ] = Tyk[uk, ]
+    }else
+    {
+      XTs[i, ] = apply(XTk[uk, ], 2, sum)
+      TTs[i, ] = apply(TTk[uk, ], 2, sum)
+      Tys[i, ] = apply(Tyk[uk, ], 2, sum)
+    }
+    u2 = (i - 1) * 2 + (1 : 2)
+    AtS[i, ] = c(crossprod(A21[u2, ], SS[u2, ]))
+  }
+  
+  bedfile = paste0(PlinkFile, ".bed")
+  bimfile = paste0(PlinkFile, ".bim")
+  
+  totalSNPs = data.table::fread(bimfile, header = FALSE); totalSNPs = totalSNPs[, 1]
+  
+  if(length(totalSNPs) > 2e3)
+  {
+    cat("Too many SNPs in the PLINK file! We randomly select 2000 SNPs.\n")
+    
+    random_SNPs = sample(totalSNPs, 2000)
+    
+    SNPIDfile = system.file("PairwiseIBD", "temp", package = "GRAB")
+    SNPIDfile = paste0(SNPIDfile, "/tempSNPID", sample(1:1e9, 1), ".txt")
+    
+    data.table::fwrite(data.frame(random_SNPs), file = SNPIDfile, 
+                       row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
+    
+    GenoMatInfo = GRAB.ReadGeno(GenoFile = bedfile,
+                                SampleIDs = SubjID,
+                                control = list(IDsToIncludeFile = SNPIDfile,
+                                               ImputeMethod = "mean"))
+    file.remove(SNPIDfile)
+    
+  }else
+  {
+    GenoMatInfo = GRAB.ReadGeno(GenoFile = bedfile,
+                                SampleIDs = SubjID,
+                                control = list(AllMarkers = TRUE,
+                                               ImputeMethod = "mean"))
+  }
+  
+  lambdaObs = c()
+  for (i in 1:nrow(GenoMatInfo$markerInfo))
+  {
+    si = GenoMatInfo$GenoMat[, i]; mu = mean(si)/2
+    
+    if(mu > 0.05)
+    {
+      snp2 = rep(si, each = 2)
+      H1 = matrix(crossprod(si, XTs), nx, 2)
+      H2 = snp2 * SS
+      AtH = matrix(crossprod(si, AtS), nx, 2)
+      R = H1 - AtH
+      Cfix = solve(Q, R)
+      Cran = H2 - A21 %*% Cfix
+      GtG = matrix(crossprod(si ^ 2, TTs), 2, 2)
+      Gty = matrix(crossprod(si, Tys), 2, 1)
+      V = GtG - crossprod(H1, Cfix) - crossprod(H2, Cran)
+      v = Gty - crossprod(H1, sol) - crossprod(H2, blups)
+      
+      lambda = V[1,2]/V[1,1]
+      
+      lambdaObs = c(lambdaObs, lambda)
+    }
+  }
+  
+  if(length(lambdaObs) > 1e2)
+  {
+    lambda = mean(lambdaObs, na.rm = TRUE)
+    cat("lambda:\t", lambda, "\n")
+  }else
+  {
+    stop("Less than 100 common SNPs (MAF > 0.05) in the PLINK file!\n")
+  }
+  
+  cat("Calculate model residuals...\n")
+  
+  coeffs = summary(null_model)$coefficients[,1]; 
+  coeffs = c(coeffs[1], coeffs[Envcolname], coeffs[CovaColname], -1)
+  update_residuals = - as.numeric(XY %*% coeffs)
+  
+  uk = 0
+  for(i in 1:n)
+  {
+    if(i %% 10000 == 0) cat(i, "\n")
+    
+    ki = k[i]
+    uk = max(uk) + 1:ki
+    
+    if(ki > 1)
+    {
+      tempmatrix = Matrix::Diagonal(k[i]) * sig^2 + TT[uk,] %*% G %*% t(TT[uk,])
+      
+      update_residuals[uk] = as.numeric(solve(tempmatrix, update_residuals[uk]))
+    }else
+    {
+      tempmatrix = sig^2 + t(TT[uk,]) %*% G %*% TT[uk,]
+      
+      update_residuals[uk] = as.numeric(solve(tempmatrix, update_residuals[uk]))
+    }
+  }
+  
+  Resid_data = Pheno_data %>% select(all_of(SubjIDColname)) %>% mutate(Resid = update_residuals * (TT[, 2] - lambda))
+  colnames(Resid_data) = c("SubjID", "Resid")
+  Resid_data = Resid_data %>% group_by(SubjID) %>% summarize(Resid = sum(Resid)) %>% ungroup()
+  
+  output = SPAGRM.NullModel(ResidMatFile = Resid_data, SparseGRMFile = SparseGRMFile, PairwiseIBDFile = PairwiseIBDFile, control = control)
+  
+  return(output)
 }
 
 make.block.GRM = function(graph, 
